de05a6fc3a877278f79b64b6c1566d0a74b2977c
snakata <majime.nekodanshi@gmail.com>
2018-09-18 21:34:07 +0900

    Androidビルド用のスクリプトを追加

---
+++ Assets/Editor/ApplicationBuild.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using UnityEditor;
+
+public static class ApplicationBuild
+{
+    public static void AndroidBuild()
+    {
+        var levels = EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+        BuildPipeline.BuildPlayer(levels, "./application.apk", BuildTarget.Android, BuildOptions.None);
+    }
+}
