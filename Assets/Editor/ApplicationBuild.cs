d566a628419451aa6c4fd7ca801d58b5879dde28
snakata <majime.nekodanshi@gmail.com>
2018-09-18 22:57:51 +0900

    デバッグ版とリリース版を切り替える仕組みを実装

--- Assets/Editor/ApplicationBuild.cs
+++ Assets/Editor/ApplicationBuild.cs
@@ -3,12 +3,28 @@ using UnityEditor;
 
 public static class ApplicationBuild
 {
+    private static BuildOptions _buildOptions = BuildOptions.None;
+
     public static void AndroidBuild()
     {
         var levels = EditorBuildSettings.scenes
             .Where(scene => scene.enabled)
             .Select(scene => scene.path)
             .ToArray();
-        BuildPipeline.BuildPlayer(levels, "./application.apk", BuildTarget.Android, BuildOptions.None);
+        ResolveCommandLineArgs();
+        BuildPipeline.BuildPlayer(levels, "./application.apk", BuildTarget.Android, _buildOptions);
+    }
+
+    private static void ResolveCommandLineArgs()
+    {
+        foreach (var command in System.Environment.GetCommandLineArgs())
+        {
+            switch (command)
+            {
+                case "-development":
+                    _buildOptions = BuildOptions.Development;
+                    break;
+            }
+        }
     }
 }
