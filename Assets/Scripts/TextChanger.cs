95f42813bc01fac1649dd3916af8c50c9b08deb1
snakata <majime.nekodanshi@gmail.com>
2018-09-18 22:41:57 +0900

    デバッグ用とリリース用の処理を追加

---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text = null;
+
+    private void Start()
+    {
+#if DEBUG
+        _text.text = "Debug";
+#else
+        _text.text = "Release";
+#endif
+    }
+
+}
